6491570dc0378e4b1ff63a66aa054f93c2b91386
U5877271@anu.edu.au <Miyal@Lanmixues-MacBook-Air.local>
2018-02-18 12:43:24 +1100

    Explore multiple variables

--- Lesson 8 Explore Many Variables.R
+++ Lesson 8 Explore Many Variables.R
@@ -25,3 +25,25 @@ ggplot(aes(x = volume, y = price), data = diamonds) +
     geom_point(aes(color = clarity))
     scale_fill_brewer(type = 'div')
   
+# Proportion of Friendships Initiated
+library(ggplot2)
+pf <- read.csv('pseudo_facebook.tsv', sep = '\t')
+names(pf)
+
+pf$prop_initiated <-  ifelse(pf$friend_count == 0, 0, pf$friendships_initiated / pf$friend_count)
+
+# prop_initiated vs. tenure
+ggplot(aes(x = tenure, y = prop_initiated), data = pf) +
+  geom_line(aes(color = year_joined), stat = 'summary', fun.y = median)
+
+# Smoothing prop_initiated vs. tenure
+ggplot(aes(x = tenure, y = prop_initiated), data = pf) +
+  geom_line(aes(color = year_joined), stat = 'summary', fun.y = median) +
+  geom_smooth(method = 'lm', color = 'red')
+
+# Price/Carat Binned, Faceted, & Colored
+ggplot(aes(x = cut, y = price /  carat),
+       data = diamonds) +
+  facet_wrap( ~ clarity) +
+  geom_point(aes(color = color), alpha = 1 / 20) + 
+  scale_fill_brewer(type = 'div')
